40766d6c42dc9410cee3f8dc0fd4775661ff3b4c
majid172 <luthfullahhilmajid172@gmail.com>
2024-01-21 21:36:14 +0600

    user verification

--- application/resources/views/presets/default/user/auth/authorization/email.blade.php
+++ application/resources/views/presets/default/user/auth/authorization/email.blade.php
@@ -1,34 +1,37 @@
 @extends($activeTemplate .'layouts.frontend')
 @section('content')
 <div class="container">
-    <div class="d-flex justify-content-center">
-        <div class="verification-code-wrapper">
-            <div class="verification-area">
-                <h5 class="pb-3 text-center border-bottom">@lang('Verify Email Address')</h5>
-                <form action="{{route('user.verify.email')}}" method="POST" class="submit-form">
-                    @csrf
-                    <p class="verification-text">@lang('A 6 digit verification code sent to your email address'): {{
-                        showEmailAddress(auth()->user()->email) }}</p>
-
-                    @include($activeTemplate.'components.verification_code')
-
-                    <div class="mb-3">
-                        <button type="submit" class="btn btn--base w-100">@lang('Save')</button>
-                    </div>
-
-                    <div class="mb-3">
-                        <p>
-                            @lang('If you don\'t get any code'), <a href="{{route('user.send.verify.code', 'email')}}">
-                                @lang('Try again')</a>
-                        </p>
-
-                        @if($errors->has('resend'))
-                        <small class="text-danger d-block">{{ $errors->first('resend') }}</small>
-                        @endif
-                    </div>
-                </form>
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-7 col-xl-6">
+            <div class="shadow-lg px-5 py-4 mb-5 bg-body rounded ">
+                <div class="verification-area">
+                    <h5 class="border-bottom pb-3 text-primary">@lang('Email Verification')</h5>
+                    <form action="{{route('user.verify.email')}}" method="POST" class="submit-form">
+                        @csrf
+                        <p class="verification-text">{{showEmailAddress(auth()->user()->email) }} @lang(' address will receive a six-digit verification number'): </p>
+    
+                        @include($activeTemplate.'components.verification_code')
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary w-100">@lang('Verify')</button>
+                        </div>
+    
+                        <div class="mb-3">
+                            <p>
+                                @lang('Should you receive no code'), <a href="{{route('user.send.verify.code', 'email')}}">
+                                    @lang('Try again')</a>
+                            </p>
+    
+                            @if($errors->has('resend'))
+                            <small class="text-danger d-block">{{ $errors->first('resend') }}</small>
+                            @endif
+                        </div>
+                    </form>
+                </div>
             </div>
+           
         </div>
     </div>
 </div>
 @endsection
+
+
--- application/resources/views/presets/default/user/auth/authorization/sms.blade.php
+++ application/resources/views/presets/default/user/auth/authorization/sms.blade.php
@@ -1,30 +1,35 @@
 @extends($activeTemplate .'layouts.frontend')
 @section('content')
+
 <div class="container">
-    <div class="d-flex justify-content-center">
-        <div class="verification-code-wrapper">
-            <div class="verification-area">
-                <h5 class="pb-3 text-center border-bottom">@lang('Verify Mobile Number')</h5>
-                <form action="{{route('user.verify.mobile')}}" method="POST" class="submit-form">
-                    @csrf
-                    <p class="verification-text">@lang('A 6 digit verification code sent to your mobile number') : +{{
-                        showMobileNumber(auth()->user()->mobile) }}</p>
-                    @include($activeTemplate.'components.verification_code')
-                    <div class="mb-3">
-                        <button type="submit" class="btn btn--base w-100">@lang('Save')</button>
-                    </div>
-                    <div class="form-group">
-                        <p>
-                            @lang('If you don\'t get any code'), <a href="{{route('user.send.verify.code', 'phone')}}"
-                                class="forget-pass"> @lang('Try again')</a>
-                        </p>
-                        @if($errors->has('resend'))
-                        <br />
-                        <small class="text-danger">{{ $errors->first('resend') }}</small>
-                        @endif
-                    </div>
-                </form>
+    <div class="row justify-content-center">
+        <div class="col-md-8 col-lg-7 col-xl-6">
+            <div class="shadow-lg px-5 py-4 mb-5 bg-body rounded ">
+                <div class="verification-area">
+                    <h5 class="border-bottom pb-3 text-primary">@lang('SMS Verification')</h5>
+                    <form action="{{route('user.verify.mobile')}}" method="POST" class="submit-form">
+                        @csrf
+                        <p class="verification-text">+{{
+                            showMobileNumber(auth()->user()->mobile) }} @lang('number will receive a six-digit verification number'): </p>
+    
+                        @include($activeTemplate.'components.verification_code')
+                        <div class="mb-3">
+                            <button type="submit" class="btn btn-primary w-100">@lang('Verify')</button>
+                        </div>
+    
+                        <div class="mb-3">
+                            <p>
+                                @lang('Should you receive no code'), <a href="{{route('user.send.verify.code', 'phone')}}" class="forget-pass">@lang('Try again')</a>
+                            </p>
+    
+                            @if($errors->has('resend'))
+                            <small class="text-danger d-block">{{ $errors->first('resend') }}</small>
+                            @endif
+                        </div>
+                    </form>
+                </div>
             </div>
+           
         </div>
     </div>
 </div>
