a7ccef154af16814e6e4bdbbe65a75b727ec6428
Majid Khan <luthfullahhilmajid172@gmail.com>
2024-03-16 00:52:20 +0600

    user sidebar

--- application/resources/views/presets/default/includes/sidebar.blade.php
+++ application/resources/views/presets/default/includes/sidebar.blade.php
@@ -12,7 +12,7 @@
                         <i class="las la-book-reader me-1 fs-4"></i>@lang('Purchase Course')
                     </a>
                 </li>
-               
+
                 <li class="list-group-item dropdown">
                     <a href="javascript:void(0)" class="nav-link dropdown-toggle text-primary " data-toggle="dropdown" data-placement="right" title="PayIn">
                         <i class="las la-money-bill-wave-alt me-1 fs-4"></i>@lang('Fund')
@@ -20,22 +20,22 @@
                     <div class="dropdown-menu " aria-labelledby="navbarDropdown">
                         <a class="dropdown-item text-primary" href="{{route('user.deposit')}}"><i class="las la-wallet me-2 fs-5"></i>@lang('Add Money')</a>
                         <a class="dropdown-item text-primary" href="{{route('user.deposit.history')}}"><i class="las la-history me-2 fs-5 "></i>@lang(' Fund History')</a>
-                    
+
                     </div>
                 </li>
-                
+
                 <li class="list-group-item dropdown">
                     <a href="javascript:void(0)" class="nav-link dropdown-toggle text-primary" data-toggle="dropdown" data-placement="right" title="Payout">
                         <i class="las la-money-check-alt me-1 fs-4"></i>@lang('Payout')
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <!-- Dropdown items go here -->
-                        <a class="dropdown-item text-primary" href="{{route('user.withdraw')}}"><i class="las la-wallet me-2 fs-5"></i>@lang('Withdraw Money')</a>
-                        <a class="dropdown-item text-primary" href="{{route('user.withdraw.history')}}"><i class="las la-history me-2 fs-5 "></i>@lang('History')</a>
+                        <a class="dropdown-item text-primary" href="{{route('user.withdraw')}}"><i class="las la-wallet me-2 fs-5"></i>@lang('Payout Money')</a>
+                        <a class="dropdown-item text-primary" href="{{route('user.withdraw.history')}}"><i class="las la-history me-2 fs-5 "></i>@lang('Payout History')</a>
                         <!-- Add more items as needed -->
                     </div>
                 </li>
-                
+
                 <li class="list-group-item">
                     <a href="{{route('user.logout')}}" class="nav-link text-primary" data-toggle="tooltip" data-placement="right" title="logout">
                         <i class="las la-sign-out-alt me-1 fs-4"></i>@lang('Logout')
