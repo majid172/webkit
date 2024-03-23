b4ea1d77beb047916df0a77a6796ade1f8a28e9e
majid172 <luthfullahhilmajid172@gmail.com>
2024-03-23 16:31:48 +0600

    episode toggle

--- application/resources/views/presets/default/user/episode/list.blade.php
+++ application/resources/views/presets/default/user/episode/list.blade.php
@@ -14,51 +14,60 @@
                 </div>
                 @endif
 
-                <div class="shadow p-3 mb-5 bg-body rounded">
-                    <ul class="list-group list-group-flush">
+                    <div class="accordion accordion-flush" id="accordionFlushExample">
                         @forelse ($episodes as $item)
-                        <li class="list-group-item d-flex justify-content-between align-items-start py-3">
-                            <div class=" ms-2 me-auto">
-                                <div class="fw-bold">
-                                    <a href="{{route('user.episode.details',$item->id)}}" class="text-primary">{{ucfirst($item->title)}}</a>
+                            @php
+                                $duration = ''; // Initialize duration variable
+                                $path = (getFilePath('episode').'/' . @$item->file_path .'/'. @$item->file);
+                                $file = $getID3->analyze($path);
+
+                                // Check if 'playtime_seconds' key exists in the $file array
+                                if (isset($file['playtime_seconds'])) {
+                                    $playtime_seconds = (int)$file['playtime_seconds']; // Convert to integer
+                                    $duration = date('H:i:s', $playtime_seconds);
+                                }
+                            @endphp
+                            <div class="shadow p-3 mb-3 bg-body rounded">
+                                <div class="accordion-item ">
+                                    <h2 class="accordion-header" id="flush-heading_{{$item->id}}">
+                                        <button class="accordion-button collapsed fw-bold text-secondary" type="button"
+                                                data-bs-toggle="collapse" data-bs-target="#flush-collapse_{{$item->id}}" aria-expanded="false" aria-controls="flush-collapse_{{$item->id}}">
+                                            <i class="las la-play-circle text-primary fs-1"></i><a href="{{route('user.episode.details',$item->id)}}" class="text-primary">  {{ucfirst($item->title)}} <span
+                                                    class="badge bg-primary mx-3"
+                                                >{{
+                               $duration }}</span></a>
+                                        </button>
+                                    </h2>
+
+                                    <div id="flush-collapse_{{$item->id}}" class="accordion-collapse collapse"
+                                         aria-labelledby="flush-heading_{{$item->id}}"
+                                         data-bs-parent="#accordionFlushExample">
+                                        <div class="accordion-body">{{$item->description}}</div>
+                                    </div>
                                 </div>
-                                <p>
-                                    @php
-                                        $duration = ''; // Initialize duration variable
-
-                                        $path = (getFilePath('episode').'/' . @$item->file_path .'/'. @$item->file);
-                                        $file = $getID3->analyze($path);
-
-                                        // Check if 'playtime_seconds' key exists in the $file array
-                                        if (isset($file['playtime_seconds'])) {
-                                            $playtime_seconds = (int)$file['playtime_seconds']; // Convert to integer
-                                            $duration = date('H:i:s', $playtime_seconds);
-                                        }
-                                    @endphp
-                                    <span class="badge bg-primary">{{ $duration }}</span>
-
-                                </p>
-                                <a href="{{route('user.episode.details',$item->id)}}" class="text-secondary">{{Str::limit($item->description, 200, '...')}}</a>
                             </div>
-                            <div>
-                                <a href="{{route('user.episode.details',$item->id)}}"><i class="las la-play-circle text-primary fs-1"></i></a>
-                            </div>
-                        </li>
-
                         @empty
-                        <li class="list-group-item d-flex justify-content-center align-items-center">
-                            <div>
-                                <img src="{{asset('assets/images/no_episode.png')}}" alt="no_episode">
+                            <div class="accordion accordion-flush" id="accordionFlushExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="flush-headingOne">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                            @lang('No episodes are available')
+                                        </button>
+                                    </h2>
+                                </div>
                             </div>
-                            <span class="text-danger text-center">
-                                @lang('No episodes are available')</span>
-                        </li>
-                        @endforelse
 
-                    </ul>
-                </div>
+                        @endforelse
+                    </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+@push('style')
+    <style>
+        .show{
+            width: 100% !important;
+        }
+    </style>
+@endpush
